1ba51b1c050983c6b5a8987bcb3550a35b679b4d
Leila <britezneira.leila@gmail.com>
2024-09-09 19:17:04 +0200

    Feat : add phrase photographer in the card

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -13,15 +13,16 @@ function photographerTemplate(data) {
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
-        const p = document.createElement( 'p' );
         const location = document.createElement( 'p' );
+        const phrase = document.createElement( 'p' );
         h2.textContent = name;
         location.textContent = city + ", " + country;
+        phrase.textContent = tagline;
         article.appendChild(img);
         article.appendChild(h2);
         article.appendChild(location);
-        article.appendChild(p);
+        article.appendChild(phrase);
         return (article);
     }
-    return { name, picture, city, country, getUserCardDOM }
+    return { name, picture, city, country, tagline, getUserCardDOM }
 }
