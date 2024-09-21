f954b4075883913ea8fc0499f419c31315c925f6
Leila <britezneira.leila@gmail.com>
2024-09-22 00:45:17 +0200

    Feat : Add Photographer data

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -3,3 +3,20 @@
 let params = new URL(document.location).searchParams;
 const id = params.get("id"); 
 
+async function getPhotographerData(id) {
+    try {
+        const response = await fetch('../../data/photographers.json');
+        if (!response.ok) {
+            throw new Error('Error reading the JSON file');
+        }
+        const data = await response.json();
+        // Filter photographer with the ID from the URL
+        const photographer = data.photographers.find(p => p.id == id);
+        console.log(photographer);
+        return photographer;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+getPhotographerData(id);
